eae826c01a5f55b541151b48833427f7163c4e45
R37r0b0y <113555835+R37r0b0y@users.noreply.github.com>
2025-08-03 20:36:08 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ def get_word():
 
 
 def play(word):
-    word_completion = '*' * len(word)  # строка, содержащая символы _ на каждую букву задуманного слова
+    word_completion = '*' * len(word)  # строка, содержащая символы * на каждую букву задуманного слова
     guessed = False
     guessed_letters = []  # список уже названных букв
     print('Давайте играть в угадайку слов!')
